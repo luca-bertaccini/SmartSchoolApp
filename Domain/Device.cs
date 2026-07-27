08cae1b999b605cbc00b2c152beaa88f69af7da8
luca-bertaccini <luca.bertaccini.stud@ispascalcomandini.it>
2026-07-27 16:17:54 +0200

    device

---
+++ Domain/Device.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Device
+    {
+        public Guid DeviceId { get; set; }
+        public string Name { get; set; }
+        public DeviceType DeviceType { get; set; }
+        public DeviceStatus Status { get; set; }
+        public Guid SchoolId { get; set; }
+        public Guid RoomId { get; set; }
+        public DateTime InstalledAt { get; set; }
+        public Specifications Specifications { get; set; }
+
+        public Device(string name, Specifications specifications)
+        {
+            DeviceId = Guid.NewGuid();
+            SchoolId = Guid.NewGuid();
+            RoomId = Guid.NewGuid();
+            InstalledAt = DateTime.UtcNow;
+            Status = DeviceStatus.Off;
+            Specifications = specifications;
+        }
+
+
+    }
+}
---
+++ Domain/School.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class School
+    {
+        public Guid SchoolId { get; set; }
+        public string Name { get; set; }
+        public Guid OwnerId { get; set; }
+
+        private School()
+        {
+            SchoolId = Guid.NewGuid();
+            OwnerId = Guid.NewGuid();
+        }
+
+        public School CreateSchool() => new School();
+    }
+}
