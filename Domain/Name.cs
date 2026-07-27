052ee22a9ed4fa6e4bd1f71a7c86a80e2ef0cb41
Francesco-Corbara <francesco.corbara.stud@ispascalcomandini.it>
2026-07-27 16:18:29 +0200

    Started Woking On School&Membership

---
+++ Domain/Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public record Name 
+    {
+        public string Value { get; }
+
+        public Name(string value) 
+        {
+            if(string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Il nome non può essere vuoto", nameof(value));
+        } 
+    }
+} 
---
+++ Domain/SchoolMembership.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public class SchoolMembership
+    {
+        public Guid SchoolId { get; set; }   // Id della scuola e dello studente
+        public Guid UserID { get; set; }
+        public string Role { get; set; }
+
+        public SchoolMembership(string role)
+        {
+            
+            Role = role;
+            Guid SchoolID = Guid.NewGuid();
+            Guid UserID = Guid.NewGuid();
+        }
+    }
+}
